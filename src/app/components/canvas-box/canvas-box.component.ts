b658d570ab19b429bc31938d27796cd83d8953b0
DigitalCrafting <k.kasprzyk2@gmail.com>
2023-11-25 20:17:54 +0100

    Glb file loader and orbit controls

--- src/app/components/canvas-box/canvas-box.component.ts
+++ src/app/components/canvas-box/canvas-box.component.ts
@@ -1,6 +1,9 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import * as THREE from 'three';
+import {Group} from 'three';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 @Component({
   selector: 'app-canvas-box',
@@ -20,6 +23,8 @@ export class CanvasBoxComponent implements OnInit {
     const canvasBox = document.getElementById("canvasBox");
     const scene = new THREE.Scene();
 
+    scene.background = new THREE.Color();
+
     const material = new THREE.MeshToonMaterial();
 
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
@@ -31,17 +36,47 @@ export class CanvasBoxComponent implements OnInit {
     pointLight.position.z = 2;
     scene.add(pointLight);
 
-    const box = new THREE.Mesh(
-      new THREE.BoxGeometry(1.5, 1.5, 1.5),
-      material
-    );
+    const loader = new GLTFLoader();
 
-    const torus = new THREE.Mesh(
-      new THREE.TorusGeometry(5, 1.5, 16, 100),
-      material
-    );
+    const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+    scene.add( light );
+    let islandModel: Group;
 
-    scene.add(torus, box);
+    loader.load(
+      '/assets/foxs_islands.glb',
+      (model) => {
+        islandModel = model.scene;
+        scene.add(model.scene);
+        model.animations;
+        model.scene;
+        model.scenes;
+        model.cameras;
+        model.asset;
+      },
+      (xhr) => {
+
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+      },
+      // called when loading has errors
+      (error) => {
+
+        console.log('An error happened');
+
+      }
+    )
+
+    // const box = new THREE.Mesh(
+    //   new THREE.BoxGeometry(1.5, 1.5, 1.5),
+    //   material
+    // );
+    //
+    // const torus = new THREE.Mesh(
+    //   new THREE.TorusGeometry(5, 1.5, 16, 100),
+    //   material
+    // );
+
+    // scene.add(torus, box);
 
     const canvasSizes = {
       width: window.innerWidth,
@@ -54,8 +89,9 @@ export class CanvasBoxComponent implements OnInit {
       0.001,
       1000
     );
-    camera.position.z = 30;
+    camera.position.z = 50;
     scene.add(camera);
+
 
     if (!canvasBox) {
       return;
@@ -64,6 +100,10 @@ export class CanvasBoxComponent implements OnInit {
     const renderer = new THREE.WebGLRenderer({
       canvas: canvasBox,
     });
+
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.update();
+
     renderer.setClearColor(0xe232222, 1);
     renderer.setSize(canvasSizes.width, canvasSizes.height);
 
@@ -84,16 +124,21 @@ export class CanvasBoxComponent implements OnInit {
       const elapsedTime = clock.getElapsedTime();
 
       // Update animation objects
-      box.rotation.x = elapsedTime;
-      box.rotation.y = elapsedTime;
-      box.rotation.z = elapsedTime;
+      // box.rotation.x = elapsedTime;
+      // box.rotation.y = elapsedTime;
+      // box.rotation.z = elapsedTime;
+      //
+      // torus.rotation.x = -elapsedTime;
+      // torus.rotation.y = -elapsedTime;
+      // torus.rotation.z = -elapsedTime;
 
-      torus.rotation.x = -elapsedTime;
-      torus.rotation.y = -elapsedTime;
-      torus.rotation.z = -elapsedTime;
+      if (islandModel) {
+        islandModel.rotation.y += 0.01;
+      }
 
       // Render
       renderer.render(scene, camera);
+      controls.update();
 
       // Call animateGeometry again on the next frame
       window.requestAnimationFrame(animateGeometry);
